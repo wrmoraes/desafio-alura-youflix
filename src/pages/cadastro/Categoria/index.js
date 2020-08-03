47a7ce1fb6152d402e5430a5ff0b45e6d2d05a9a
Willian Moraes <willian_200@hotmail.com>
2020-08-03 18:46:01 -0300

    Ajustes do cadastro de categoria e inclusão de server json;

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,32 +1,107 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
 function CadastroCategoria() {
-    return (
-        <PageDefault>
-            <h1>Cadastro de Categoria</h1>
+  const categoriaInicial = {
+    nome: '',
+    descricao: '',
+    cor: '',
+  };
 
-            <form>
+  const [categoria, setCategoria] = useState(categoriaInicial);
+  const [categorias, setCategorias] = useState([]);
 
-                <label>
-                    Nome da Categoria:
-                    <input
-                        type="text"
-                    />
-                </label>
+  function setValue(name, value) {
+    setCategoria({
+      ...categoria,
+      [name]: value,
+    });
+  }
 
-                <button>
-                    Cadastrar
-                </button>
-            </form>
+  function handlerOnChangeValue({ target }) {
+    setValue(
+      target.getAttribute('name'),
+      target.value,
+    );
+  }
 
+  useEffect(() => {
+    const URL = 'http://localhost:8080/categorias';
 
-            <Link to="/">
-                Ir para home
-            </Link>
-        </PageDefault>
-    )
+    fetch(URL).then(async (serverResponse) => {
+      const response = await serverResponse.json();
+      setCategorias([...response]);
+    });
+  }, []);
+
+  return (
+    <PageDefault>
+      <h1>
+        Cadastro de Categoria:
+        {categoria.nome}
+      </h1>
+
+      <form onSubmit={function handleSubmit(infosDoEvento) {
+        infosDoEvento.preventDefault();
+        setCategorias([
+          ...categorias,
+          categoria,
+        ]);
+
+        setCategoria(categoriaInicial);
+      }}
+      >
+
+        <FormField
+          label="Nome da Categoria"
+          name="nome"
+          type="text"
+          value={categoria.nome}
+          onChange={handlerOnChangeValue}
+        />
+
+        <FormField
+          label="Descrição"
+          name="descricao"
+          type="textarea"
+          value={categoria.descricao}
+          onChange={handlerOnChangeValue}
+        />
+
+        <FormField
+          label="Cor"
+          name="cor"
+          type="color"
+          value={categoria.cor}
+          onChange={handlerOnChangeValue}
+        />
+
+        <Button>
+          Cadastrar
+        </Button>
+      </form>
+      {categorias.length === 0 && (
+        <div>
+          Carregando...
+        </div>
+      )}
+
+      <ul>
+        {categorias.map((categoriaItem, indice) => (
+          <li key={`${categoriaItem}-${indice}`}>
+            {categoriaItem.nome}
+          </li>
+        ))}
+      </ul>
+
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  );
 }
 
 export default CadastroCategoria;
